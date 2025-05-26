25e027349e9f7a2bb62e45640fa170e1d867467a
valeria <golovatova.vm@gmail.com>
2025-05-26 11:14:39 +0700

    Изменения в некоторых функциях. Были добавлены виртуальные функции,чтобы избежать ошибок при использование к другим деревьям.

--- Lab2/BinaryTree/BinaryTree/BinaryTree.h
+++ Lab2/BinaryTree/BinaryTree/BinaryTree.h
@@ -24,28 +24,31 @@ public:
 	};
 	BinaryTree() = default;
 	BinaryTree(const BinaryTree& other);
-	~BinaryTree();
+	virtual ~BinaryTree();
 	Node* root() const;
 	void clear();
 	bool isEmpty() const;
 	int Height(Node* root) const;
 	int Count(Node* root) const;
-	int findMinimum(Node* root);
-	int findMaximum(Node* root);
-	Node* addNode(Node* root, int key);
+	virtual int findMinimum(Node* root);
+	virtual int findMaximum(Node* root);
+	virtual Node* addNode(Node* root, int key);
 	void addKey(int key);
-	bool removeNode(Node* root, Node* node);
+	virtual bool removeNode(Node* root, Node* node);
 	bool removeNodeKey(int key);
 	Node* nlrSearch(Node* root, int key)const;
-	std::vector<int> ascendingKey() const;
+	virtual std::vector<int> ascendingKey() const;
 	BinaryTree& operator=(const BinaryTree& other);
 	void printHorizontal(Node* root, int marginLeft = 0, int levelSpacing = 5) const;
 private:
 	Node* CopyTree(Node* root);
 	void clear(Node* node);
 	Node* findLeaf(Node* node) const;
-	Node* findParent(Node* root, Node* node) const;
-	void getKeys(Node* root, std::vector<int>& keys) const;
+	
+	
+protected:
+	virtual Node* findParent(Node* root, Node* node) const;
+	virtual void getKeys(Node* root, std::vector<int>& keys) const;
 	Node* m_root = nullptr;
 
 };
